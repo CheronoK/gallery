3b784fdfc50151f88f8b4bf94c34f8337ac8fd40
CheronoK <64804869+CheronoK@users.noreply.github.com>
2022-11-24 13:53:24 +0300

    Update _config.js

---
+++ _config.js
@@ -0,0 +1,9 @@
+var config = {}
+
+// Update to have your correct username and password
+config.mongoURI = {
+    production: 'mongodb+srv://cherok:chero123@ipcluster.nhbemru.mongodb.net/?retryWrites=true&w=majority',
+    development: 'mongodb+srv://cherok:chero123@ipcluster.nhbemru.mongodb.net/?retryWrites=true&w=majority',
+    test: 'mongodb+srv://cherok:chero123@ipcluster.nhbemru.mongodb.net/?retryWrites=true&w=majority',
+}
+module.exports = config;
