851d6063cd95802331c9e6c3b740d64683148218
CheronoK <64804869+CheronoK@users.noreply.github.com>
2022-11-25 01:38:59 +0300

    Update _config.js

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://cherok:chero123@kcluster.2jkdkri.mongodb.net/?retryWrites=true&w=majority',
-    development: 'mongodb+srv://cherok:chero123@kcluster.2jkdkri.mongodb.net/?retryWrites=true&w=majority',
-    test: 'mongodb+srv://cherok:chero123@kcluster.2jkdkri.mongodb.net/?retryWrites=true&w=majority',
+    production: 'mongodb+srv://cherono:chero123@kcluster.2jkdkri.mongodb.net/?retryWrites=true&w=majority',
+    development: 'mongodb+srv://cherono:chero123@kcluster.2jkdkri.mongodb.net/?retryWrites=true&w=majority',
+    test: 'mongodb+srv://cherono:chero123@kcluster.2jkdkri.mongodb.net/?retryWrites=true&w=majority',
 }
 module.exports = config;
